245b8148940fc131ea8f15c464df77ad7da93cf9
cpicanco <cpicanco42@gmail.com>
2017-08-29 22:22:30 -0300

    add pupil tasks, pupil client and zmq client

---
+++ pupil.tasks.pas
@@ -0,0 +1,105 @@
+{
+  pupil-fpc
+  Copyright (C) 2014-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+  The present file is distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
+unit Pupil.Tasks;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+function PupilSubscriberTask(Args : Pointer) : PtrInt;
+
+const
+  forever = False;
+
+var
+  PupilRequestHost : PChar = 'tcp://127.0.0.1:50020';
+  PupilSubscribeHost : string = 'tcp://127.0.0.1:';
+
+implementation
+
+uses SimpleMsgPack, Pupil.Client, zmq, zmq.helpers;
+
+function PupilSubscriberTask(Args: Pointer): PtrInt;
+var
+  context, subscriber :  Pointer;
+  zmq_message : zmq_msg_t;
+  filter : string = SUB_ALL_NOTIFICATIONS;
+
+  Serializer :  TSimpleMsgPack;
+  MsgTopic : string;
+  MsgPackage : TMemoryStream;
+  PupilMessage : TPupilMessage;
+
+
+  procedure DumpPupilMessage(PupilMessage: TPupilMessage);
+  var j : integer;
+  begin
+    with PupilMessage do
+    begin
+      WriteLn('------------------------------------');
+      WriteLn('Topic:', Topic);
+      WriteLn('TopicCount:', IntToStr(Payload.Count));
+      with Payload do
+        for j := 0 to Count -1 do
+          WriteLn(Items[j].Key, ':', Items[j].Value);
+    end;
+  end;
+
+begin
+  Result := PtrInt(0);
+  try
+    context := zmq_ctx_new;
+    subscriber := zmq_socket(context, ZMQ_SUB);
+    zmq_connect(subscriber, PChar(PupilSubscribeHost));
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, @filter[1], Length(filter));
+    Serializer := TSimpleMsgPack.Create;
+    MsgPackage := TMemoryStream.Create;
+    repeat
+      zmq_message := Default(zmq_msg_t);
+      zmq_msg_init(zmq_message);
+      zmq_msg_recv(zmq_message, subscriber, 0);
+      try
+        // frame 1 is a string
+        SetString(MsgTopic, PAnsiChar(zmq_msg_data(zmq_message)), zmq_msg_size(zmq_message));
+        PupilMessage.Topic := MsgTopic;
+
+        // frame 2 is MsgPack serialization
+        MsgPackage.Clear;
+        if zmq_msg_more(zmq_message) = 1 then
+        begin
+          zmq_msg_close(zmq_message);
+          zmq_msg_init(zmq_message);
+          zmq_msg_recv(zmq_message, subscriber, 0);
+
+          MsgPackage.WriteBuffer(zmq_msg_data(zmq_message)^, zmq_msg_size(zmq_message));
+          MsgPackage.Position := 0;
+
+          Serializer.Clear;
+          Serializer.DecodeFromStream(MsgPackage);
+          PupilMessage.Payload := Serializer;
+        end;
+        DumpPupilMessage(PupilMessage);
+      finally
+        zmq_msg_close(zmq_message);
+      end;
+    until forever;
+  finally
+    Serializer.Free;
+    MsgPackage.Free;
+    zmq_close(subscriber);
+    zmq_ctx_shutdown(context);
+  end;
+end;
+
+end.
+
---
+++ zmq.client.pas
@@ -0,0 +1,219 @@
+{
+  pupil-fpc
+  Copyright (C) 2014-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+  The present file is distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
+unit ZMQ.Client;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses Classes;
+
+type
+  { TMultiPartMessage }
+
+  TMultiPartMessage = record
+    MsgPackage : TMemoryStream;
+    MsgTopic : string;
+  end;
+
+  { TMultiPartMessageReceived }
+
+  TMultiPartMessageReceived = procedure(AResponse: TMultiPartMessage) of object;
+
+  { TZMQSubThread }
+
+  TZMQSubThread = class(TThread)
+  private
+    FMultipartMessage : TMultiPartMessage;
+    FContext : Pointer;
+    FSubscriber : Pointer;
+    FOnMultipartMessageReceived: TMultiPartMessageReceived;
+    procedure MultipartMessageReceived;
+  protected
+    procedure Execute; override;
+  public
+    constructor Create(ASubHost : string; CreateSuspended: Boolean = True);
+    destructor Destroy; override;
+    procedure Subscribe(AFilter : string);
+    procedure Unsubscribe(AFilter : string);
+    property OnMultiPartMessageReceived: TMultiPartMessageReceived
+      read FOnMultipartMessageReceived write FOnMultipartMessageReceived;
+  end;
+
+  { TResponseReceivedEvent }
+
+  TResponseReceivedEvent = procedure(ARequest, AResponse: String) of object;
+
+  { TZMQReqThread }
+
+  TZMQReqThread = class(TThread)
+  private
+    FResponse,
+    FRequest : string;
+    FContext : Pointer;
+    FRequester : Pointer;
+    FRTLEvent: PRTLEvent;
+    FOnResponseReceived: TResponseReceivedEvent;
+    procedure ResponseReceived; inline;
+  protected
+    procedure Execute; override;
+    procedure SendRequest(ARequest : string; Blocking : Boolean = False);
+    property OnResponseReceived: TResponseReceivedEvent read FOnResponseReceived write FOnResponseReceived;
+  public
+    constructor Create(AHost : string; CreateSuspended: Boolean = True);
+    destructor Destroy; override;
+  end;
+
+implementation
+
+uses zmq, zmq.helpers;
+
+{ TZMQSubThread }
+
+procedure TZMQSubThread.MultipartMessageReceived;
+begin
+  if Assigned(FOnMultipartMessageReceived) then FOnMultipartMessageReceived(FMultipartMessage);
+end;
+
+procedure TZMQSubThread.Execute;
+var
+  zmq_message : zmq_msg_t;
+begin
+  while not Terminated do
+    begin
+      zmq_message := Default(zmq_msg_t);
+      zmq_msg_init(zmq_message);
+      with FMultipartMessage do
+      try
+        // frame 1 is a string
+        zmq_msg_recv(zmq_message, FSubscriber, 0);
+        SetString(MsgTopic, PAnsiChar(zmq_msg_data(zmq_message)), zmq_msg_size(zmq_message));
+
+        // frame 2 is MsgPack serialization
+        if zmq_msg_more(zmq_message) = 1 then
+        begin
+          zmq_msg_close(zmq_message);
+          zmq_msg_init(zmq_message);
+          zmq_msg_recv(zmq_message, FSubscriber, 0);
+          MsgPackage := TMemoryStream.Create;
+          MsgPackage.WriteBuffer(zmq_msg_data(zmq_message)^, zmq_msg_size(zmq_message));
+        end;
+
+        // deserialization is done outside
+        Queue(@MultipartMessageReceived);
+      finally
+        zmq_msg_close(zmq_message);
+      end;
+    end;
+end;
+
+constructor TZMQSubThread.Create(ASubHost: string; CreateSuspended: Boolean);
+var
+  host : PChar;
+begin
+  host := PChar('tcp://' + ASubHost);
+  FreeOnTerminate := True;
+
+  FContext := zmq_ctx_new;
+  FSubscriber := zmq_socket(FContext, ZMQ_SUB);
+  zmq_connect(FSubscriber, host);
+  inherited Create(CreateSuspended);
+end;
+
+destructor TZMQSubThread.Destroy;
+begin
+  zmq_close(FSubscriber);
+  zmq_ctx_shutdown(FContext);
+  inherited Destroy;
+end;
+
+procedure TZMQSubThread.Subscribe(AFilter: string);
+begin
+  if AFilter = '' then
+    zmq_setsockopt(FSubscriber, ZMQ_SUBSCRIBE, nil, 0)
+  else
+    zmq_setsockopt(FSubscriber, ZMQ_SUBSCRIBE, @AFilter[1], Length(AFilter));
+end;
+
+procedure TZMQSubThread.Unsubscribe(AFilter: string);
+begin
+  if AFilter = '' then
+    zmq_setsockopt(FSubscriber, ZMQ_UNSUBSCRIBE, nil, 0)
+  else
+    zmq_setsockopt(FSubscriber, ZMQ_UNSUBSCRIBE, @AFilter[1], Length(AFilter));
+end;
+
+{ TZMQReqThread }
+
+constructor TZMQReqThread.Create(AHost: string; CreateSuspended: Boolean);
+var
+  host : PChar;
+begin
+  host := PChar('tcp://' + AHost);
+  FResponse := '';
+  FRequest := '';
+  FreeOnTerminate := True;
+  FRTLEvent := RTLEventCreate;
+
+  FContext := zmq_ctx_new;
+  FRequester := zmq_socket(FContext, ZMQ_REQ);
+  zmq_connect(FRequester, host);
+  inherited Create(CreateSuspended);
+end;
+
+destructor TZMQReqThread.Destroy;
+begin
+  zmq_close(FRequester);
+  zmq_ctx_shutdown(FContext);
+  RTLEventDestroy(FRTLEvent);
+  inherited Destroy;
+end;
+
+procedure TZMQReqThread.ResponseReceived;
+begin
+  if Assigned(FOnResponseReceived) then FOnResponseReceived(FRequest, FResponse);
+end;
+
+procedure TZMQReqThread.Execute;
+var
+  ARequest: string;
+begin
+  while not Terminated do
+    begin
+      RTLeventWaitFor(FRTLEvent);
+
+      // make a local copy to synchronize shared variables
+      ARequest := FRequest;
+      SendString(FRequester, ARequest);
+
+      // wait for response
+      FResponse := RecvShortString(FRequester);
+
+      // queued in the main thread (TApplication)
+      Queue(@ResponseReceived); // returns immediately
+    end;
+end;
+
+procedure TZMQReqThread.SendRequest(ARequest: string; Blocking: Boolean);
+begin
+  FRequest := ARequest;
+  if Blocking then
+    begin
+      SendString(FRequester, ARequest);
+      FResponse := RecvShortString(FRequester);
+      ResponseReceived;
+    end
+  else
+    RTLeventSetEvent(FRTLEvent);
+end;
+
+
+
+end.
