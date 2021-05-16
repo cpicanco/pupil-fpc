9c3c090a6c46672c7f913fe906711aa0c7245884
Rafael Picanço <cpicanco@users.noreply.github.com>
2021-05-16 15:15:38 -0300

    Update pupil.client.pas license header (#1)
    * Update pupil.client.pas, pupil.tasks.pas, zmq.client.pas headers

--- pupil.client.pas
+++ pupil.client.pas
@@ -2,7 +2,7 @@
   pupil-fpc
   Copyright (C) 2014-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
 
-  The present file is distributed under the terms of the GNU Lesser General Public License (LGPL v3.0).
+  The present file is distributed under the terms of the GNU General Public License (GPL v3.0)
 
   You should have received a copy of the GNU General Public License
   along with this program. If not, see <http://www.gnu.org/licenses/>.
